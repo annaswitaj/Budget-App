8b995d8cb92cb7b4a66650fe4ea27a81f1e23fb7
annaswitaj <annaswitaj.sw@gmail.com>
2023-02-17 15:08:09 +0100

    add styles

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,35 @@
+import { createClient } from '@supabase/supabase-js';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 import React from 'react';
-import Input from '../Input';
+import { useNavigate } from 'react-router-dom';
+
+const supaBase = createClient(
+  'https://uvkgygthopvimmeyerho.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2a2d5Z3Rob3B2aW1tZXllcmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyOTgyNDgsImV4cCI6MTk5MTg3NDI0OH0.dFduv1hQ3JovQaDPkLc3igucMGt4Ysw05MWrr_CQmEk'
+);
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  supaBase.auth.onAuthStateChange(async (event) => {
+    if (event !== 'SIGNED_OUT') {
+      //forward to success URL
+      navigate('/success');
+    } else {
+      //forward to http://127.0.0.1:5173/
+      navigate('/');
+    }
+  });
   return (
-    <div>
-      <Input label='Email address' />
-      <Input label='Password' />
+    <div className='App'>
+      <header className='App-header'>
+        <Auth
+          supabaseClient={supaBase}
+          appearance={{ theme: ThemeSupa }}
+          theme='dark'
+          providers={['google']}
+        />
+      </header>
     </div>
   );
 };
--- src/pages/SuccessPage.tsx
+++ src/pages/SuccessPage.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const supaBase = createClient(
+  'https://uvkgygthopvimmeyerho.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2a2d5Z3Rob3B2aW1tZXllcmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyOTgyNDgsImV4cCI6MTk5MTg3NDI0OH0.dFduv1hQ3JovQaDPkLc3igucMGt4Ysw05MWrr_CQmEk'
+);
 
 const SuccessPage = () => {
-  return <div>SuccessPage</div>;
+  const [user, setUser] = useState({});
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function getUserData() {
+      await supaBase.auth.getUser().then((value) => {
+        if (value.data?.user) {
+          console.log(value.data.user);
+
+          setUser(value.data.user);
+        }
+      });
+    }
+    getUserData();
+  }, []);
+
+  async function signOutUser() {
+    const { error } = await supaBase.auth.signOut();
+    navigate('/');
+  }
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        {Object.keys(user).length !== 0 ? (
+          <>
+            <h1>Success</h1>
+            <button onClick={() => signOutUser()}>Sign Out</button>
+          </>
+        ) : (
+          <>
+            <h1>User is not logged in</h1>
+            <button
+              onClick={() => {
+                navigate('/');
+              }}
+            >
+              Go back home
+            </button>
+          </>
+        )}
+      </header>
+    </div>
+  );
 };
 
 export default SuccessPage;
